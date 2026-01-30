eeb72df475b38c41fb16a0c70a4c142a996603a5
Shariq Torres <shariq.torres@gmail.com>
2026-01-30 03:12:30 -0800

    added --keys option

--- server.py
+++ server.py
@@ -9,17 +9,20 @@ import utils
 parser = argparse.ArgumentParser(prog="Tribes Keyserver", description="Tribes KeyServer")
 parser.add_argument("--start-prod", action='store_true')
 parser.add_argument("--start-debug", action='store_true')
+parser.add_argument("--keys", action='store_true')
 
 args = parser.parse_args()
 load_dotenv()
+
+if args.keys is True:
+  if len(os.listdir(utils.get_keyfile_directory())) == 0:
+      utils.generate_keys(os.getenv('USERNAME'), os.getenv('EMAIL'), utils.get_keyfile_directory())
 
 if args.start_prod is True:
   from waitress import serve
   from server.app import app
   os.environ['MODE'] = 'PROD'
   print(f"Running production server on port {os.environ['PORT']}")
-  if len(os.listdir(utils.get_keyfile_directory())) == 0:
-    utils.generate_keys(os.getenv('USERNAME'), os.getenv('EMAIL'), utils.get_keyfile_directory())
   serve(app, host='0.0.0.0', port=os.environ['PORT'])
 
 if args.start_debug is True:
